157bd671ce8772f757aa954795c83c64f29fb3bb
bss8 <borislav.sabotinov@gmail.com>
2021-07-23 20:25:52 -0500

    Boris: updated VPP class diagrams. Updated index and ViewsController.

--- src/main/java/edu/txstate/library/controllers/ViewsController.java
+++ src/main/java/edu/txstate/library/controllers/ViewsController.java
@@ -1,2 +1,12 @@
-package edu.txstate.library.controllers;public class ViewsController {
+package edu.txstate.library.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewsController {
+    @RequestMapping(value={"/"})
+    public String indexPage() {
+        return "index";
+    }
 }
